62a843226bb8cd35bf8531ca8ddaab65ca26015f
Hagai Cohen <hagaic@mellanox.com>
2015-12-07 10:05:46 +0200

    splitted installer into optional steps

--- install.sh
+++ install.sh
@@ -1,34 +1,63 @@
 #!/bin/bash
 
-cd ~
+function opt_install() {
+	read -p "Would you like to $1? (Y/N)" -n 1 -r
+	echo    # (optional) move to a new line
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		$2
+	fi
+}
 
-# Clone the repository
-git clone --recursive https://github.com/DxCx/WorkEnv .dxcx_workenv
-cd .dxcx_workenv
+function install_plugins() {
+	${ENV_DIR_PATH}/tmux/tpm/bin/install_plugins
+	vim +BundleInstall +qall
+}
 
-# Build configuration files
-echo source `pwd`/vim/vimrc.vim > ~/.vimrc
-echo source-file \${ENV_DIR_PATH}/tmux/tmux.conf > ~/.tmux.conf
-echo export PATH=\"${PATH}\" > ~/.zshrc
-echo export ENV_DIR_PATH=`pwd` >> ~/.zshrc
+function config_git() {
+	git config --global core.editor "vim"
+	git config --global merge.tool "vimdiff"
+	git config --global color.ui auto
+	git config --global branch.autosetuprebase always
+}
 
-# Load ENV_DIR_PATH
-source ~/.zshrc
-# Enable zshrc config loading
-echo source \${ENV_DIR_PATH}/terminal/zshrc >> ~/.zshrc
+function set_default_zsh() {
+	if [[ -x /bin/zsh ]]; then
+		chsh ${LOGNAME} -s /bin/zsh
+		exec /bin/zsh
+	fi
+}
+
+function load_enviroment() {
+	cd ~
+
+	# Clone the repository
+	git clone --recursive https://github.com/DxCx/WorkEnv .dxcx_workenv
+	cd .dxcx_workenv
+
+	# Build config for vim
+	echo source `pwd`/vim/vimrc.vim > ~/.vimrc
+
+	# Build config for tmux
+	echo source-file \${ENV_DIR_PATH}/tmux/tmux.conf > ~/.tmux.conf
+
+	# Build config for zsh
+	echo export PATH=\"${PATH}\" > ~/.zshrc
+	echo export ENV_DIR_PATH=`pwd` >> ~/.zshrc
+	# Load ENV_DIR_PATH
+	source ~/.zshrc
+	# Enable zshrc config loading
+	echo source \${ENV_DIR_PATH}/terminal/zshrc >> ~/.zshrc
+}
+
+# Loading initial enviorment
+load_enviroment
 
 # Install plugins
-${ENV_DIR_PATH}/tmux/tpm/bin/install_plugins
-vim +BundleInstall +qall
+opt_install "Download and install all plugins now" install_plugins
 
 # Config git
 git config fetch.recurseSubmodules true
-git config --global core.editor "vim"
-git config --global merge.tool "vimdiff"
-git config --global color.ui auto
-git config --global branch.autosetuprebase always
+opt_install "Configure git" config_git
 
-# Change zsh to default shell
-chsh ${LOGNAME} -s /bin/zsh
-
-exec /bin/zsh
+# Change zsh to default shell (Keep last)
+opt_install "Use ZSH as default shell" set_default_zsh
