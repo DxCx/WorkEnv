cdbe6bdeb40db64bfd221196ee2b779998fc94ee
Hagai Cohen <hagai.co@gmail.com>
2016-12-09 10:48:28 +0200

    fix package checker for brew

--- bin/install.sh
+++ bin/install.sh
@@ -95,7 +95,7 @@ function check_n_install_os_deps()
 		QCMD="pacman -Qq | grep \"\$i\" || pacman -Qqg | grep \"\$i\""
 	elif which brew &> /dev/null; then
 		DEPENDENCIES=(${BREW_DEPENDS[@]})
-		QCMD="brew list"
+		QCMD="brew list --versions \$1"
 	else
 		echo "ERROR: Couldn't find package manager"
 		opt_oper "Exit" true bail_error
