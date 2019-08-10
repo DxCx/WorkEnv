f3d0e8ba40c2274bd8c528acae317efaa9d8ee3a
Hagai Cohen <hagai.co@gmail.com>
2019-08-10 20:30:48 +0300

    neovim: Initial integration with Plug
    Signed-off-by: Hagai Cohen <hagai.co@gmail.com>

--- bin/update.sh
+++ bin/update.sh
@@ -5,3 +5,9 @@ ${ENV_DIR_PATH}/tmux/tpm/bin/update_plugins all
 vim "+call map(dein#check_clean(), \"delete(v:val, 'rf')\")" +qall
 vim "+call dein#install()" +qall
 vim "+call dein#update()" +qall
+
+nvim "+PlugUpgrade" +qall
+nvim "+PlugClean" +qall
+nvim "+PlugInstall" +qall
+nvim "+PlugUpdate" +qall
+nvim "+UpdateRemotePlugins" +qall
